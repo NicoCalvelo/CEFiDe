5a606ea76860dc665cf820f54799f2ac8b3ad2ed
MichoyTito <48106968+MichoyTito@users.noreply.github.com>
2019-04-19 16:56:32 -0300

    19/04

---
+++ CEFIDE/Assets/Scripts/Paneles/Registrarse.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Registrarse : MonoBehaviour
+{
+    [SerializeField]
+    private Animator regisAnim;
+
+
+    public void onClickPaso2()
+     {
+        regisAnim.SetBool("backPaso2", false);
+        regisAnim.SetBool("paso2", true);
+     }
+    public void onClcikPaso3()
+    {
+        regisAnim.SetBool("backPaso3", false);
+        regisAnim.SetBool("paso2", false);
+        regisAnim.SetBool("paso3", true);
+    }
+    public void onClcikBackPaso3()
+    {
+        regisAnim.SetBool("paso3", false);
+        regisAnim.SetBool("backPaso3", true);
+    }
+    public void onClickBackPaso2()
+    {
+        regisAnim.SetBool("paso2", false);
+        regisAnim.SetBool("backPaso3", false);
+        regisAnim.SetBool("backPaso2", true);
+    }
+}
