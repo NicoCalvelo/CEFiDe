ef26155bac14e78689b86a7ee46c71240c815d80
MichoyTito <48106968+MichoyTito@users.noreply.github.com>
2019-04-24 20:01:40 -0300

    24/ 04

--- CEFIDE/Assets/Scripts/Paneles/Registrarse.cs
+++ CEFIDE/Assets/Scripts/Paneles/Registrarse.cs
@@ -19,15 +19,26 @@ public class Registrarse : MonoBehaviour
         regisAnim.SetBool("paso2", false);
         regisAnim.SetBool("paso3", true);
     }
-    public void onClcikBackPaso3()
+    public void onClickPaso4()
     {
+        regisAnim.SetBool("backPaso4", false);
         regisAnim.SetBool("paso3", false);
-        regisAnim.SetBool("backPaso3", true);
+        regisAnim.SetBool("paso4", true);
     }
     public void onClickBackPaso2()
     {
         regisAnim.SetBool("paso2", false);
         regisAnim.SetBool("backPaso3", false);
         regisAnim.SetBool("backPaso2", true);
+    }
+    public void onClcikBackPaso3()
+    {
+        regisAnim.SetBool("paso3", false);
+        regisAnim.SetBool("backPaso3", true);
+    }
+    public void onClickBackPaso4()
+    {
+        regisAnim.SetBool("paso4", false);
+        regisAnim.SetBool("backPaso4", true);
     }
 }
