800f239df1059999d4b8fc7766a6603ff22e8f27
MichoyTito <48106968+MichoyTito@users.noreply.github.com>
2019-04-25 20:32:06 -0300

    25/04

---
+++ CEFIDE/Assets/Scripts/Paneles/homePanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class homePanel : MonoBehaviour
+{
+    [SerializeField]
+    private Animator anim_homePanel;
+
+
+    public void onClickNoticias()
+    {
+        anim_homePanel.SetBool("onClickNoticias", true);
+    }
+    public void onClcikNovedades()
+    {
+        anim_homePanel.SetBool("onClickNoticias", false);
+    }
+}
